a0d1e4585ee967725ad2bbed79ce3b2f3a77152c
Plucky <hello@plucky.lol>
2025-12-31 00:57:54 +0100

    init

---
+++ src/main/java/lol/plucky/hideScoreboard/client/MainClient.java
@@ -0,0 +1,10 @@
+package lol.plucky.hideScoreboard.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MainClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
